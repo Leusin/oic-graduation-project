c9d64117af5b4f6a3086afc32247643a6409b8f5
Leusin <leusin@naver.com>
2022-11-02 22:23:07 +0900

    design update: statusBarColor, icon image, splashActivity, ArFragment

---
+++ oic-android/sdk/build/intermediates/cxx/Debug/2a493h1s/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Documents\\oic-graduation-project-clone\\oic-graduation-project\\oic-android\\sdk\\.cxx\\Debug\\2a493h1s\\arm64-v8a" ^
+  opencv_jni_shared
