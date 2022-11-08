729e065d220ae6f259ac21936df30f0e50682bdd
minseoo2o <minseoo2o6@duksung.ac.kr>
2022-11-08 13:25:07 +0900

    단어장 연결 성공, 단어장 디자인 수정...

---
+++ oic-android/sdk/build/intermediates/cxx/Debug/1f29495n/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\lg25l\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\OIC\\final\\oic-android\\sdk\\.cxx\\Debug\\1f29495n\\arm64-v8a" ^
+  opencv_jni_shared
